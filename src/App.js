e91220cef0a0e22885167d08bcbcb47029d298ca
Nisha <nisha@Nishas-MacBook-Pro.local>
2022-07-23 01:21:35 +0530

    new commti

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import AddUser from "./component/Users/AddUser";
+import UsersList from "./component/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUserList) => {
+      return [
+        ...prevUserList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
+
